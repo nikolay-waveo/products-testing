57528087069f22873f8a26994b47a87c1c5b0464
nikolay-waveo <nikolay@waveo.com>
2021-12-06 17:46:06 +0800

    Feat: Add SubscriptionList component

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,5 +1,6 @@
 import Container from "components/Container"
 import Section from "components/Section"
+import SubscriptionList from "components/SubscriptionList"
 import Title from "components/Title"
 
 
@@ -8,9 +9,15 @@ const admin: React.FC = () => {
     <Container>
       <Title>Store Product Sync</Title>
 
-      <Section sectionTitle="Publish">Sample Text</Section>
+      <Section sectionTitle="Publish">
+        <SubscriptionList 
+          emptyListMessage="There are no subscribers." />
+      </Section>
 
-      <Section sectionTitle="Subscribe">Sample Text</Section>
+      <Section sectionTitle="Subscribe">
+        <SubscriptionList 
+          emptyListMessage="There are no subscriptions." />
+      </Section>
     </Container>
   )
 }
