77d66ba0da4a1aa331cc1a154b8f42ca52daf00d
nikolay-waveo <nikolay@waveo.com>
2021-12-06 16:50:55 +0800

    Feat: Add Section component

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,5 @@
 import Container from "components/Container"
+import Section from "components/Section"
 import Title from "components/Title"
 
 
@@ -6,6 +7,10 @@ const admin: React.FC = () => {
   return (
     <Container>
       <Title>Store Product Sync</Title>
+
+      <Section sectionTitle="Publish">Sample Text</Section>
+
+      <Section sectionTitle="Subscribe">Sample Text</Section>
     </Container>
   )
 }
