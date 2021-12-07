97bf003ffc26e9fc560c67ddac84689bd19332d7
nikolay-waveo <nikolay@waveo.com>
2021-12-07 16:09:24 +0800

    Refactor: Split List component into Item

--- components/List.tsx
+++ components/List.tsx
@@ -1,45 +1,38 @@
-import { Icon } from '@shopify/polaris';
-import { CancelSmallMinor } from '@shopify/polaris-icons';
 import React from 'react';
+import Item from './Item';
 
 interface IList {
   list: {
     name: string,
-    URL?: string,
+    id: string,
+    URL: string,
   }[],
+  updateListHandler: React.Dispatch<React.SetStateAction<{
+    name: string;
+    id: string;
+    URL: string;
+  }[]>>,
   emptyListMessage: string,
 }
 
 const List: React.FC<IList> = ({
   list,
+  updateListHandler,
   emptyListMessage,
 }) => {
  
+  const onRemove = (id: string) => {
+    const newList = list.filter((item) => item.id !== id);
+    console.log(newList)
+    updateListHandler(newList);
+  }
+
   return (
     <ul>
       { list.length > 0
-        ? list.map((item, key) => {
-          return(
-            <li 
-              key={key}
-              className="flex justify-between my-3" >
-              { item.URL 
-                ? <> 
-                  <a href={ item.URL }
-                    className="overflow-hidden">
-                    { item.name }
-                  </a> 
-                  <button
-//TODO Remove testing script
-                    onClick={() => {alert("click")}}>
-                    <Icon
-                    source={CancelSmallMinor}
-                    color="base" />
-                  </button> </>
-                : item.name }
-            </li>
-          )
-        })
+        ? list.map((item) => 
+          <Item key={item.id} item={item} onRemove={ onRemove }/>
+        )
         : <li>{ emptyListMessage }</li>
       }
     </ul>
