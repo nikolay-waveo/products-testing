d7359ea1cac56da9ce3d197efb65863b08bc6b48
nikolay-waveo <nikolay@waveo.com>
2021-12-07 10:51:38 +0800

    Feat: Add state

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -2,33 +2,32 @@ import Container from "components/Container"
 import List from "components/List"
 import Section from "components/Section"
 import Title from "components/Title"
+import { useState } from "react"
 
 
-const admin: React.FC = () => {
+const Admin: React.FC = () => {
+
+  const [incomingSubscriptions, setIncomingSubscriptions] = useState([]);
+  const [outgoingSubscriptions, setOutgoingSubscriptions] = useState([]);
+
   return (
     <Container>
       <Title>Store Product Sync</Title>
 
       <Section sectionTitle="Publish">
         <List 
-          list={[
-            {listItem: "test"},
-            {listItem: "shopify shop", listItemURL: "test.com"}
-          ]}
+          list={incomingSubscriptions}
           emptyListMessage="There are no subscribers." />
 
       </Section>
 
       <Section sectionTitle="Subscribe">
         <List 
-            list={[
-              {listItem: "test"},
-              {listItem: "shopify shop", listItemURL: "test.com"}
-            ]}
+            list={outgoingSubscriptions}
             emptyListMessage="There are no subscriptions." />
       </Section>
     </Container>
   )
 }
 
-export default admin
+export default Admin
