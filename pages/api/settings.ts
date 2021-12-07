739b81318f6328802fe8b2c75a52fe1cee30849d
dgtlmonk <dgtlmonk@gmail.com>
2021-12-07 09:29:04 +0800

    fix: (api) settings

--- pages/api/settings.ts
+++ pages/api/settings.ts
@@ -1,12 +1,27 @@
-// Fake  settings
+const settings = {
+  published: [
+    {
+      shop: "dev-subscriber.myshopify.com",
+      status: "active",
+      enabled: true,
+      inventoryLocationId: "61576970293",
+      updatedAt: "2021-12-03T05:14:10.140Z",
+    },
+  ],
+  subscribed: [],
+  publish: true,
+};
+
 export default function handler(req, res) {
   console.log("req.query ", req.query);
 
+  const isPublished = req.query?.publish;
+
   if (req.method === "POST") {
-    return res.status(200).json({ publish: true });
+    return res.status(200).json({ publish: isPublished });
   }
   // if (req.method === 'GET') { }
 
   // TODO: proxy actual api endpoint
-  return res.status(200).json({ status: "ok" });
+  return res.status(200).json(settings);
 }
