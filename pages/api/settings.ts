021c27b601254812a50e03b37a198c4668d74724
dgtlmonk <dgtlmonk@gmail.com>
2021-12-07 08:32:35 +0800

    refactor: settings api

--- pages/api/settings.ts
+++ pages/api/settings.ts
@@ -1,18 +1,4 @@
 // Fake  settings
-const settings = {
-  published: [
-    {
-      shop: "dev-subscriber.myshopify.com",
-      status: "active",
-      enabled: true,
-      inventoryLocationId: "61576970293",
-      updatedAt: "2021-12-03T05:14:10.140Z",
-    },
-  ],
-  subscribed: [],
-  publish: true,
-};
-
 export default function handler(req, res) {
   console.log("req.query ", req.query);
 
@@ -22,5 +8,5 @@ export default function handler(req, res) {
   // if (req.method === 'GET') { }
 
   // TODO: proxy actual api endpoint
-  return res.status(200).json(settings);
+  return res.status(200).json({ status: "ok" });
 }
