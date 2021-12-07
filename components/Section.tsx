019a8048f4b1f750baf4462a39c9a03c32b166f8
nikolay-waveo <nikolay@waveo.com>
2021-12-07 09:43:21 +0800

    Feat: Add react-toggle

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Toggle from 'react-toggle'
+import "react-toggle/style.css"
 import { ISection } from 'types'
+
 
 const Section: React.FC<ISection> = ({
   sectionTitle,
   children
 }) => {
+
+  const [toggleOn, setToggleOn] = useState(true)
+
   return (
     <section className="divide-y-2 divide-gray-300 divide-solid">
 
-      <h2 className="text-2xl font-semibold">{ sectionTitle }</h2>
+      <div className="flex justify-between content-center my-5">
+        <h2 className="text-2xl font-semibold">{ sectionTitle }</h2>
 
-      <div>
-        { children }
+        <Toggle 
+          defaultChecked={toggleOn}
+          icons={false}
+          onChange={() => setToggleOn(!toggleOn)} />
       </div>
+
+      { toggleOn && <div> { children } </div> }
 
     </section>
   )
