bde4fce530f2d83170698bcb087aae91e4f6e025
nikolay-waveo <nikolay@waveo.com>
2021-12-08 14:38:50 +0800

    Feat: Updated stat object

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,36 +1,29 @@
-import AddToList from "components/AddToList"
 import Container from "components/Container"
 import Section from "components/Section"
-import SubscriptionList from "components/SubscriptionList"
 import Title from "components/Title"
 import { useState } from "react"
-import { ISubscriptions } from "types"
 
+interface ISubscription {
+  subscription: {
+    name: string, 
+    id: string, 
+    URL: string, 
+    status: string,
+  },
+}
 
 const Admin: React.FC = () => {
 
-  const [incomingPendingSubs, setIncomingPendingSubs] = useState<ISubscriptions['subscriptions']>([
-    { name: "Shopify Store 1", id: "1AC", URL: "test.com" },
-    { name: "Shopify Store 2", id: "2BD", URL: "test.com" },
-    { name: "Shopify Store 3", id: "3C0", URL: "test.com" },
+  const [incomingSubs, setIncomingSubs] = useState<ISubscription['subscription'][]>([
+    { name: "Shopify Store 1", id: "1AC", URL: "test.com", status: "pending" },
+    { name: "Shopify Store 2", id: "2BD", URL: "test.com", status: "active" },
+    { name: "Shopify Store 3", id: "3C0", URL: "test.com", status: "active" },
   ]);
 
-  const [incomingSubs, setIncomingSubs] = useState<ISubscriptions['subscriptions']>([
-    { name: "Shopify Store 4", id: "6AC", URL: "test.com" },
-    { name: "Shopify Store 5", id: "5BD", URL: "test.com" },
-    { name: "Shopify Store 6", id: "4C0", URL: "test.com" },
-  ]);
-
-  const [outgoingPendingSubs, setOutgoingPendingSubs] = useState<ISubscriptions['subscriptions']>([
-    { name: "Shopify Store 7", id: "1AXC", URL: "test.com" },
-    { name: "Shopify Store 8", id: "2BXD", URL: "test.com" },
-    { name: "Shopify Store 9", id: "3CX0", URL: "test.com" },
-  ]);
-
-  const [outgoingSubs, setOutgoingSubs] = useState<ISubscriptions['subscriptions']>([
-    { name: "Shopify Store 10", id: "6A44", URL: "test.com" },
-    { name: "Shopify Store 11", id: "5B44", URL: "test.com" },
-    { name: "Shopify Store 12", id: "4C44", URL: "test.com" },
+  const [outgoingSubs, setOutgoingSubs] = useState<ISubscription['subscription'][]>([
+    { name: "Shopify Store 4", id: "1AC0", URL: "test.com", status: "pending" },
+    { name: "Shopify Store 5", id: "2BD0", URL: "test.com", status: "active" },
+    { name: "Shopify Store 6", id: "3C00", URL: "test.com", status: "active" },
   ]);
 
   const outgoingSubscriptionsHandler = (subscription: string) => {
@@ -72,41 +65,11 @@ const Admin: React.FC = () => {
       <Title>Store Product Sync</Title>
 
       <Section sectionTitle="Publish">
-        <SubscriptionList 
-          listTitle="Pending"
-          list={incomingPendingSubs}
-          ListUpdateHandler={setIncomingPendingSubs}
-          linkedList={incomingSubs}
-          LinkedListUpdateHandler={setIncomingSubs}
-          emptyListMessage="There are no subscribers." />
 
-        <SubscriptionList 
-          listTitle="Subscribers"
-          list={incomingSubs}
-          ListUpdateHandler={setIncomingSubs}
-          emptyListMessage="There are no subscribers." />
       </Section>
 
       <Section sectionTitle="Subscribe">
-        <AddToList 
-          label="Subscribe to store"
-          labelName="subscribeToStore"
-          placeholder="Add store URL"
-          addToListHandler={outgoingSubscriptionsHandler}/>
-
-        <SubscriptionList
-          listTitle="Pending" 
-          list={outgoingPendingSubs}
-          ListUpdateHandler={setOutgoingPendingSubs}
-          linkedList={outgoingSubs}
-          LinkedListUpdateHandler={setOutgoingSubs}
-          emptyListMessage="There are no subscriptions." />
-          
-        <SubscriptionList
-          listTitle="Subscriptions" 
-          list={outgoingSubs}
-          ListUpdateHandler={setOutgoingSubs}
-          emptyListMessage="There are no subscriptions." />
+  
       </Section>
     </Container>
   )
