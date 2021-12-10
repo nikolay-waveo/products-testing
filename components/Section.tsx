04af7a2d4e76cdf673a22a333090ce8b562d98bc
nikolay-waveo <nikolay@waveo.com>
2021-12-10 14:12:35 +0800

    Feat: Update Section component styles

--- components/Section.tsx
+++ components/Section.tsx
@@ -34,7 +34,7 @@ const Section: React.FC<ISection> = ({
 
       { toggleOn 
         ? <div className="col-span-2 bg-indigo-50 rounded-md overflow-hidden custom-drop-shadow" > { children } </div> 
-        : <div className="flex flex-col space-y-5 justify-center content-center p-10 col-span-2 bg-gray-50 rounded-md overflow-hidden custom-drop-shadow" > 
+        : <div className="md:flex hidden flex-col space-y-5 justify-center content-center p-10 col-span-2 bg-gray-50 rounded-md overflow-hidden custom-drop-shadow" > 
             <h3
               className="text-4xl text-gray-700 text-center">
               Your store is hiding.
