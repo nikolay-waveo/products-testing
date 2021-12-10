7bee4e4bb6290e1ec899aececdbef7fd1641122a
nikolay-waveo <nikolay@waveo.com>
2021-12-10 13:51:06 +0800

    Feat: Add onDisabled card

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import Toggle from 'react-toggle'
-import "react-toggle/style.css"
-import { ISection } from 'types'
+import React, { useState } from 'react';
+import Toggle from 'react-toggle';
+import "react-toggle/style.css";
+import { ISection } from 'types';
 
 
 const Section: React.FC<ISection> = ({
@@ -12,11 +12,18 @@ const Section: React.FC<ISection> = ({
 
   const [toggleOn, setToggleOn] = useState(true)
 
-  return (
-    <section className="max-h-full border border-gray-100">
+  const sectionDescription = "Subscriber to a published store and check on pending subscriptions."
 
-      <div className="flex justify-between content-center p-5 pt-6 bg-white">
-        <h2 className="h-10 text-3xl font-bold text-gray-800">{ sectionTitle }</h2>
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-3 gap-5">
+
+      <div className="flex justify-between content-center md:p-5 md:pt-6">
+        <div>
+          <h2 className="h-10 mb-5 text-3xl font-semibold text-gray-800">{ sectionTitle }</h2>
+
+          <span className="text-xl text-gray-500" >{ sectionDescription }</span>
+        </div>
+        
 
         { toggle && 
           <Toggle 
@@ -25,7 +32,17 @@ const Section: React.FC<ISection> = ({
             onChange={() => setToggleOn(!toggleOn)} /> }
       </div>
 
-      { toggleOn && <div className="bg-indigo-50" > { children } </div> }
+      { toggleOn 
+        ? <div className="col-span-2 bg-indigo-50 rounded-md overflow-hidden custom-drop-shadow" > { children } </div> 
+        : <div className="flex flex-col space-y-5 justify-center content-center p-10 col-span-2 bg-gray-50 rounded-md overflow-hidden custom-drop-shadow" > 
+            <h3
+              className="text-4xl text-gray-700 text-center">
+              Your store is hiding.
+            </h3>
+            <span 
+              className="text-lg text-gray-500 text-center"> Toggle ON to publish your store. </span>
+          </div> 
+      }
 
     </section>
   )
