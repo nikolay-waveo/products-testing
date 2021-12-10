7a8f0ef9c5020facffb54400f31f31a363019969
nikolay-waveo <nikolay@waveo.com>
2021-12-10 17:28:11 +0800

    Feat: Add status text styling

--- components/Item.tsx
+++ components/Item.tsx
@@ -8,6 +8,19 @@ const Item: React.FC<IItem> = ({
   onConnect,
 }) => {
 
+  let statusStyle = " ";
+
+  switch(item.status) {
+    case 'PENDING': 
+      statusStyle += "text-yellow-600"
+      break;
+    case 'ACTIVE':
+      statusStyle += "text-shopify-success"
+      break;
+    default:
+      break;
+  }
+
   return (
     <li
       className="grid grid-flow-col grid-cols-9 justify-between items-center p-5 bg-white" >
@@ -16,7 +29,7 @@ const Item: React.FC<IItem> = ({
          { item.name }
       </a>
       <span
-        className="col-span-1 text-lg text-left">
+        className={"col-span-1 text-lg text-left font-semibold" + statusStyle } >
         { item.status }
       </span>
       <span className="col-span-5"></span>
