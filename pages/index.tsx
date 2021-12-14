06e7d523459f92743b732c4f386c30184668fe58
nikolay-waveo <nikolay@waveo.com>
2021-12-14 14:55:32 +0800

    Feat: Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { AppProvider, Frame, Page } from "@shopify/polaris";
 import List from "components/List";
 import Section from "components/Section";
-import { useSettings } from "hooks/useSettings";
-import { usePublish } from "hooks/useSubscribe";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ISubscription } from "types";
 
 const Admin: React.FC = () => {
@@ -20,89 +18,6 @@ const Admin: React.FC = () => {
     { storeURL: "test.com", id: "2BD2", status: "active" },
     { storeURL: "test.com", id: "3C02", status: "active" },
   ]);
-
-  //? ----------------------------------------------------------------------------------
-
-  const shop = "dev-subscriber.myshopify.com";
-
-  const pubShop = "dev-publisher.myshopify.com";
-
-  let getPubList:any[]
-  let getSubList:any[]
-  let putSubList:any[]
-
-  let getPub:any
-  let getSub:any
-  let putSub:any
-
-  useEffect(() => {
- 
-  }, [getPub, getSub, putSub])
-
-  const {useGetShopSettings: getSettings, setShopSettings: setSettings} = useSettings()
-
-  const getData = getSettings(shop).data
-
-  try {
-    getPubList = getData.published
-    getSubList = getData.subscribed
-
-    getPub = getPubList
-      .map((item, key) => {
-      return (
-        <p key={key}>
-          {item.shop}/
-          {item.inventoryLocationId}/
-          {item.status}
-        </p>
-      )
-    })
-
-    getSub = getSubList
-      .map((item, key) => {
-      return (
-        <p key={key}>
-          {item.shop}/
-          {item.inventoryLocationId}/
-          {item.status}
-        </p>
-      )
-    })
-  } catch (error) {
-    getPub = ""
-    getSub = ""
-  }
-
-  // const setPub = setSettings(shop, {
-  //   publish: true,
-  //   published: getPubList,
-  //   subscribed: getSubList,
-  // })
-
-  const {setShopPublishSettings: setSubscribe} = usePublish();
-
-  const setSubObject = {
-    publisherShop: pubShop,
-    subscriberShop: shop,
-    accept: true,
-  }
-
-  setSubscribe(setSubObject)
-    .then((r) => { 
-      console.log(r, typeof r)
-      // r.map((item, key) => {
-      //   return (
-      //     <p key={key}>
-      //       {item.shop}/
-      //       {item.inventoryLocationId}/
-      //       {item.status}
-      //     </p>
-      //   )
-      // })  
-    }).catch(() => '')
-
-  //? ----------------------------------------------------------------------------------
-
 
   return (
     <AppProvider 
@@ -127,22 +42,6 @@ const Admin: React.FC = () => {
             title="Store Product Sync"
             fullWidth={true}
             divider >
-
-  {/* //? ---------------------------------------------------------------------------------- */}
-
-          {/* <div>
-            GET pub: { getPub }
-          </div>
-
-          <div>
-            GET sub: { getSub }
-          </div>
-
-          <div>
-            PUT sub: { putSub }
-          </div> */}
-
-  {/* //? ---------------------------------------------------------------------------------- */}
 
             <div className="grid grid-cols-1 gap-10">
               <Section 
