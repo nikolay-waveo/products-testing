a1c48d99eb25a1a9cb5ec65c4f0b79b92fe6a43c
nikolay-waveo <nikolay@waveo.com>
2021-12-14 15:22:24 +0800

    Feat: Add useSettings call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,96 @@
 import { AppProvider, Frame, Page } from "@shopify/polaris";
 import List from "components/List";
 import Section from "components/Section";
-import { useState } from "react";
+import { useSettings } from "hooks/useSettings";
+import { useEffect, useState } from "react";
 import { ISubscription } from "types";
 
 const Admin: React.FC = () => {
 
   const [incomingSubs, setIncomingSubs] = useState<ISubscription['subscription'][]>([
-    { storeURL: "test.com", id: "1ACQ", status: "pending" },
-    { storeURL: "test.com", id: "2BDQ", status: "active" },
+    // { storeURL: "test.com", id: "1ACQ", status: "pending" },
+    // { storeURL: "test.com", id: "2BDQ", status: "active" },
   ]);
 
   const [outgoingSubs, setOutgoingSubs] = useState<ISubscription['subscription'][]>([
-    { storeURL: "test.com", id: "1AC0", status: "pending" },
-    { storeURL: "test.com", id: "2BD0", status: "active" },
-    { storeURL: "test.com", id: "1AC2", status: "pending" },
-    { storeURL: "test.com", id: "2BD2", status: "active" },
-    { storeURL: "test.com", id: "3C02", status: "active" },
+    // { storeURL: "test.com", id: "1AC0", status: "pending" },
+    // { storeURL: "test.com", id: "2BD0", status: "active" },
+    // { storeURL: "test.com", id: "1AC2", status: "pending" },
+    // { storeURL: "test.com", id: "2BD2", status: "active" },
+    // { storeURL: "test.com", id: "3C02", status: "active" },
   ]);
+
+  //? ----------------------------------------------------------------------------------
+
+  //TODO
+  const shop = "dev-publisher.myshopify.com";
+  //TODO
+  
+  const {
+    useGetShopSettings: getSettings, 
+    setShopSettings: setSettings,
+  } = useSettings()
+
+  const {data, isLoading} = getSettings(shop)
+
+  //! Figure out what this is supposed to do
+  //! Aside from enable publisher permissions
+
+  const testData = {
+    "shop": "dev-subscriber-test.myshopify.com",
+    "status": "pending",
+    "enabled": true,
+    "inventoryLocationId": "62489591961",
+    "updatedAt": "2021-12-09T05:02:58.399Z"
+  }
+
+  const onSetSettings = () => {
+    setSettings(shop, {
+      publish: true,
+      subscribed: [testData],
+    }).then(r => console.log('response: ', r))
+  }
+
+  // onSetSettings()
+
+  //! =======================================
+
+  useEffect(() => {
+    // GET incoming and outgoing subscriptions
+    if(!isLoading) {
+      const incomingSubsData = data
+      .published
+      .map((storeData) => {
+        return ({
+          storeURL: storeData.shop,
+          id: storeData.inventoryLocationId,
+          status: storeData.status,
+        })
+      })
+
+      setIncomingSubs(incomingSubsData)
+
+      const outgoingSubsData = data
+      .subscribed
+      .map((storeData) => {
+        return ({
+          storeURL: storeData.shop,
+          id: storeData.inventoryLocationId,
+          status: storeData.status,
+        })
+      })
+
+      setOutgoingSubs(outgoingSubsData)
+      
+    }
+
+    // PUT outgoing subscription
+
+    // console.log(setStoreData)
+
+  }, [data, isLoading,])
+
+  //? ----------------------------------------------------------------------------------
 
   return (
     <AppProvider 
@@ -43,7 +116,7 @@ const Admin: React.FC = () => {
             fullWidth={true}
             divider >
 
-            <div className="grid grid-cols-1 gap-10">
+            <div className="grid grid-cols-1 gap-10 mb-20">
               <Section 
                 sectionTitle="Publish"
                 sectionDescription="See which stores are subscribed to you."
