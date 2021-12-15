54173a02f891020f5c9a4a0718c3328465001d77
nikolay-waveo <nikolay@waveo.com>
2021-12-15 11:38:56 +0800

    Refactor: Update property names

--- hooks/usePublish.ts
+++ hooks/usePublish.ts
@@ -1,19 +1,19 @@
 import { client } from "helpers/api-client";
 
 type TPublishProps = {
+  origin: string;
   publisherShop: string;
-  subscriberShop: string;
   accept?: boolean;
 };
 
 async function useSETShopPublishSettings(props: TPublishProps) {
   return await client.put(`api/publish`, {
     headers: {
-      "x-shopify-shop-domain": `${props.publisherShop}`,
+      "x-shopify-shop-domain": `${props.origin}`,
     },
     body: JSON.stringify({
       accept: props?.accept,
-      shop: props.subscriberShop,
+      shop: props.publisherShop,
     }),
   });
 }
@@ -21,10 +21,10 @@ async function useSETShopPublishSettings(props: TPublishProps) {
 async function useDELETEShopPublishSettings(props: Omit<TPublishProps, "accept">) {
   return await client.delete(`api/publish`, {
     headers: {
-      "x-shopify-shop-domain": `${props.publisherShop}`,
+      "x-shopify-shop-domain": `${props.origin}`,
     },
     body: JSON.stringify({
-      shop: props.subscriberShop,
+      shop: props.publisherShop,
     }),
   });
 }
