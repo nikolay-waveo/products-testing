54d32231df83d57c64b25122755c3523c582b202
nikolay-waveo <nikolay@waveo.com>
2021-12-17 11:34:43 +0800

    Feat: Add user address store

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,116 @@
 import { AppProvider, Frame, Page } from "@shopify/polaris";
 import List from "components/List";
 import Section from "components/Section";
-// import { usePublish } from "hooks/usePublish";
 import { useSettings } from "hooks/useSettings";
-import { useSubscribe } from "hooks/useSubscribe";
 import { useEffect, useState } from "react";
 import { ISubscription } from "types";
 
 const Admin: React.FC = () => {
 
+  //change to PublishedToList
   const [incomingSubs, setIncomingSubs] = useState<ISubscription['subscription'][]>([]);
 
+  //change to SubscribedToList
   const [outgoingSubs, setOutgoingSubs] = useState<ISubscription['subscription'][]>([]);
 
   //? ----------------------------------------------------------------------------------
 
   //TODO
-  const pubShop = "dev-publisher.myshopify.com";
+  const pubShop = "dev-publisher.myshopify.com"
 
   const subShop = "dev-subscriber.myshopify.com"
+
+  const tubShop = "testing-pub-dev.myshopify.com" //Pub Sub
   //TODO
   
+
+  // GET lists
   const {
     useGETShopSettings: getSettings, 
     useSETShopSettings: setSettings,
   } = useSettings()
 
-  const {data, isLoading} = getSettings(pubShop)
+  const {data, isLoading} = getSettings(tubShop)
+
+  // SET user to Publisher mode
+
+  setSettings(pubShop, {
+    publish: true
+  })
+
+  // Subscribe to other user
 
   // const {
-  //   setShopPublishSettings: setPublish,
+  //   useSETShopSubscribeSettings: setSubscribe,
+  //   useDELETEShopSubscribeSettings: deleteSubscribe,
+  // } = useSubscribe();
+
+  //? Adds to Origin SubscribedTo list and Destination PublishedTo list
+  // //! Find out how to get inventoryLocationId
+  // setSubscribe({
+  //   origin: tubShop,
+  //   subscriberShop: pubShop,
+  //   accept: true,
+  //   id: '00000000001'
+  // })
+  // .then(r => console.log("SUB SET", r))
+
+  //? Deletes from SubscribedTo list
+  // deleteSubscribe({
+  //   origin: tubShop,
+  //   subscriberShop: pubShop,
+  // }).then(r => console.log("SUB DEL", r))
+
+  
+  // const {
+  //   useSETShopPublishSettings: setPublish,
+  //   useDELETEShopPublishSettings: deletePublish,
   // } = usePublish();
 
-  const {
-    useSETShopSubscribeSettings: setSubscribe,
-  } = useSubscribe();
-
-  setSubscribe({
-    publisherShop: pubShop,
-    subscriberShop: "joel-dev-subscriber.myshopify.com",
-    accept: true,
-  }).then(r => console.log("Subscription res: ", r))
-
-  //! Figure out what this is supposed to do
-  //! Aside from enable publisher permissions
-
-  // const testData = {
-  //   "shop": subShop,
-  //   "inventoryLocationId": "88888888888",
-  // }
-
-  // const onSetSettings = () => {
-  //   setSettings(pubShop, {
-  //     publish: true,
-  //     published: [testData],
-  //   }).then(r => console.log('response: ', r))
-  // }
-
-  // console.log(testData)
-
-  // onSetSettings()
+  //? Confirms subscription
+  // setPublish({
+  //   origin: pubShop,
+  //   publisherShop: subShop,
+  //   accept: true,
+  // })
+  // .then(r => console.log("PUB SET", r))
 
   //! =======================================
 
   useEffect(() => {
     // GET incoming and outgoing subscriptions
     if(!isLoading) {
-      console.log(data.published, data.subscribed)
       const incomingSubsData = data
-      .published
-      .map((storeData) => {
+      .published?.map(({
+        shop,
+        inventoryLocationId,
+        status,
+      }) => {
         return ({
-          storeURL: storeData.shop,
-          id: storeData.inventoryLocationId,
-          status: storeData.status,
+          storeURL: shop,
+          id: inventoryLocationId,
+          status: status,
         })
-      })
+      }) || []
 
       setIncomingSubs(incomingSubsData)
 
       const outgoingSubsData = data
-      .subscribed
-      .map((storeData) => {
+      .subscribed?.map(({
+        shop,
+        inventoryLocationId,
+        status,
+      }) => {
         return ({
-          storeURL: storeData.shop,
-          id: storeData.inventoryLocationId,
-          status: storeData.status,
+          storeURL: shop,
+          id: inventoryLocationId,
+          status: status,
         })
-      })
+      }) || []
 
       setOutgoingSubs(outgoingSubsData) 
     }
-
   }, [data, isLoading, ])
-
-  // useEffect(() => {
-  //   client
-  //     .put("https://shopify.perkd.io/products-pubsub-app-dev/subscribe", {
-  //       headers: {
-  //         "x-shopify-shop-domain": `${pubShop}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         inventoryLocationId: "00000000000",
-  //         shop: subShop,
-  //       }),
-  //     })
-  //     .then((res) => console.log(" response ", res));
-  // }, []);
-
-  //? ----------------------------------------------------------------------------------
 
   return (
     <AppProvider 
