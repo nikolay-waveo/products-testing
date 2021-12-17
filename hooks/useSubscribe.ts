d842000c6ba37124b212fbeea9c86b4ee402ce21
nikolay-waveo <nikolay@waveo.com>
2021-12-17 09:12:21 +0800

    Feat: Add delete functionality to subscibe hook

--- hooks/useSubscribe.ts
+++ hooks/useSubscribe.ts
@@ -3,16 +3,34 @@ import { client } from "helpers/api-client";
 type TSubscribeProps = {
   origin: string;
   subscriberShop: string;
-  accept: boolean;
+  accept?: boolean;
+  id: string;
 };
 
+const { API_ENDPOINT } = process.env;
+
+//! Needs inventoryLocationId
 async function useSETShopSubscribeSettings(props: TSubscribeProps) {
-  return await client.put(`api/subscribe`, {
+  return await client.put(`${API_ENDPOINT}/subscribe`, {
     headers: {
       "x-shopify-shop-domain": `${props.origin}`,
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       accept: props?.accept,
+      shop: props.subscriberShop,
+      inventoryLocationId: props.id,
+    }),
+  });
+}
+
+async function useDELETEShopSubscribeSettings(props: Omit<TSubscribeProps, "accept">) {
+  return await client.delete(`${API_ENDPOINT}/subscribe`, {
+    headers: {
+      "x-shopify-shop-domain": `${props.origin}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
       shop: props.subscriberShop,
     }),
   });
@@ -21,5 +39,6 @@ async function useSETShopSubscribeSettings(props: TSubscribeProps) {
 export function useSubscribe() {
   return {
     useSETShopSubscribeSettings,
+    useDELETEShopSubscribeSettings,
   };
 }
