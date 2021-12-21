ea8a9542a4307533e522ca11f6d0d5a5f538c9d3
nikolay-waveo <nikolay@waveo.com>
2021-12-21 09:18:47 +0800

    Feat: Add TestingCard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { AppProvider, Card, Frame, Page, Tabs, TextStyle } from "@shopify/polaris";
+import { AppProvider, Frame, Page } from "@shopify/polaris";
 import List from "components/List";
 import Section from "components/Section";
+import TestingCard from "components/testingCard";
 import { useSettings } from "hooks/useSettings";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ISubscription } from "types";
 
 const Admin: React.FC = () => {
@@ -79,58 +80,6 @@ const Admin: React.FC = () => {
     }
   }, [data, isLoading, ])
 
-  //? ---------------------------------------------------------------
-  // For testing convenience
-
-    const [selected, setSelected] = useState(0);
-
-    const handleTabChange = useCallback(
-      (selectedTabIndex) => {
-        setSelected(selectedTabIndex)
-        switch(selectedTabIndex) {
-          case 0: 
-            return setUser("dev-subscriber.myshopify.com")
-          case 1: 
-            return setUser("dev-publisher.myshopify.com")
-          case 2: 
-            return setUser("testing-pub-dev.myshopify.com")
-          default: 
-            return setUser("dev-subscriber.myshopify.com")
-        }
-      },
-      [],
-    );
-
-    const tabs = [
-      {
-        id: 'dev-subscribe',
-        content: 'Publish and Subscribe',
-        panelID: 'dev-subscribe-content',
-      },
-      {
-        id: 'dev-publish',
-        content: 'Publish',
-        panelID: 'dev-publish-content',
-      },
-      {
-        id: 'dev-test',
-        content: 'Subscribe Only',
-        panelID: 'dev-test-content',
-      },
-    ];
-
-    const testingComponent = (
-      <Card>
-        <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
-          <Card.Section title={tabs[selected].content}>
-            <p>Selected store: <TextStyle variation="strong">{user}</TextStyle></p>
-          </Card.Section>
-        </Tabs>
-      </Card>
-    )
-
-  //? ---------------------------------------------------------------
-
   return (
     <AppProvider 
       i18n={{
@@ -159,7 +108,7 @@ const Admin: React.FC = () => {
 
   {/* //? --------------------------------------------------------------- */}
 
-              {testingComponent}
+              <TestingCard user={user} setUser={setUser} />
 
   {/* //? --------------------------------------------------------------- */}
 
