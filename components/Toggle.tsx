4bdc4ffef9d47348273d9bbf9f667a31b0b6f1c6
nikolay-waveo <nikolay@waveo.com>
2021-12-23 10:32:40 +0800

    Feat: Create Toggle component

---
+++ components/Toggle.tsx
@@ -0,0 +1,73 @@
+import * as polaris from '@shopify/polaris';
+import { Button, Card, Heading, TextStyle } from '@shopify/polaris';
+import React from 'react';
+
+declare type Variation = 'positive' | 'negative' | 'strong' | 'subdued' | 'code';
+
+declare type ToggleState = {
+  title?: string,
+  content: string,
+  contentStyle?: Variation,
+  primaryAction: polaris.ComplexAction,
+  secondaryAction?: polaris.ComplexAction,
+  primary?: boolean,
+  destructive?: boolean,
+}
+
+interface IToggle {
+  activated: boolean,
+  onAction: () => void,
+  onActivate: ToggleState,
+  onDeactivate: ToggleState,
+}
+
+const Toggle: React.FC<IToggle> = ({
+  activated,
+  onAction,
+  onActivate,
+  onDeactivate,
+}) => {
+
+  const state = activated ? onActivate : onDeactivate
+  const {
+    title,
+    content,
+    contentStyle,
+    primaryAction,
+    secondaryAction,
+    primary,
+    destructive,
+  } = state
+
+  const onPrimaryAction = () => {
+    primaryAction.onAction()
+    onAction()
+  }
+
+  return (
+    <Card sectioned >
+      <div className="flex justify-between items-center">
+        <div className="flex flex-col">
+          <Heading>{title}</Heading>
+          <span className="mt-4">
+            <TextStyle variation={contentStyle}>
+              {content}
+            </TextStyle>
+          </span>
+        </div>
+        <div className="flex w-max gap-3">
+          { secondaryAction && 
+            <Button onClick={secondaryAction.onAction}>{secondaryAction.content}</Button> }
+          <Button 
+            primary={primary} 
+            destructive={destructive} 
+            onClick={onPrimaryAction}>
+              {primaryAction.content}
+          </Button>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+export default Toggle
